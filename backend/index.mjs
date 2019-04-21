5db06e17a7763dfd988917d2e59eb5cfa7d06827
Jorge <jorgeojedalopez92@gmail.com>
2019-04-21 18:45:14 +0200

    Added table for list users

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import apiController from './domain/apicontroller'
-import cors from 'cors'
+// import cors from 'cors'
 
 dotenv.config()
 const { PORT, PUBLIC_URL } = process.env
@@ -11,14 +11,11 @@ const router = express.Router()
 
 app.use(`${PUBLIC_URL}`, router)
 
-app.use(
-  cors({
-    maxAge: 1800,
-    origin: 'localhost',
-    optionsSuccessStatus: 200
-  })
-)
-app.options('*', cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 router.use(express.json())
 
--- src/components/UserManagment.vue
+++ src/components/UserManagment.vue
@@ -1,15 +1,25 @@
 <template lang="pug">
   .container-fluid
+    h1 {{ probando }}
     table.table.table-striped
       thead
         tr
           th(scope='col') #
-          th(scope='col') First
-          th(scope='col') Last
-          th(scope='col') Handle
+          th(scope='col') dni
+          th(scope='col') email
+          th(scope='col') nombre
+          th(scope='col') telefono
+          th(scope='col') direccion
+          th(scope='col') fecha nacimiento
       tbody
         tr(v-for="(user, index) in userList" :key="index")
-          td {{user.nombre}}
+          td {{ user.id }}
+          td {{ user.dni }}
+          td {{ user.email }}
+          td {{ user.nombre }}
+          td {{ user.telefono }}
+          td {{ user.direccion }}
+          td {{ user.fecha_nacimiento }}
 </template>
 
 <script>
@@ -17,17 +27,21 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 export default {
-  data: {
-    userList: []
+  data() {
+    return {
+      probando: 'texto normal',
+      userList: []
+    }
   },
-  mounted: function() {
+  mounted() {
     this.getUsers()
   },
   methods: {
     getUsers(){
-      axios.get('localhost:3000/apaz/v1/usuarios/')
-        .then(function (response) {
-            console.log(response.data);
+      axios.get('http://localhost:3000/apaz/v1/usuarios')
+        .then((response) => {
+          this.userList = response.data
+          console.log(this.userList)
         })
         .catch(function (error) {
             console.log(error);
