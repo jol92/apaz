3f745c51680b458b3a59b7fc2b9d2a02051d364f
Jorge <jorgeojedalopez92@gmail.com>
2019-04-22 11:05:54 +0200

    Changed headers ORM

--- backend/domain/apicontroller.mjs
+++ backend/domain/apicontroller.mjs
@@ -2,6 +2,12 @@ import knex from '../infraestructure/knex'
 
 const tableName = 'usuarios'
 const apicontroller = app => {
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+
   app.get('/usuarios', (req, res, next) => knex(tableName).select('*').then(data => res.send(data)))
 
   app.get('/usuarios/:usuarioId', (req, res, next) => {
--- backend/index.mjs
+++ backend/index.mjs
@@ -11,12 +11,6 @@ const router = express.Router()
 
 app.use(`${PUBLIC_URL}`, router)
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
 router.use(express.json())
 
 apiController(router)
