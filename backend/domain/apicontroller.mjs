a673283aab7fc1f04620623e3610ba622f575cf6
Jorge <jorgeojedalopez92@gmail.com>
2019-05-01 13:37:37 +0200

    New methods to display and add users

--- backend/domain/apicontroller.mjs
+++ backend/domain/apicontroller.mjs
@@ -1,4 +1,5 @@
 import knex from '../infraestructure/knex'
+import bcrypt from 'bcrypt'
 
 const apicontroller = app => {
   app.use(function (req, res, next) {
@@ -8,14 +9,40 @@ const apicontroller = app => {
   })
 
   // Usuarios
-  app.get('/usuarios', (req, res, next) => knex('usuarios').join('provincias', 'usuarios.id_provincia', '=', 'provincias.id_provincia').select('usuarios.*', 'provincias.provincia').then(data => res.send(data)))
+  app.get('/usuarios', (req, res, next) => knex('usuarios').join(
+    'provincias', 'usuarios.id_provincia', '=', 'provincias.id_provincia'
+  )
+    .join(
+      'tipos_vivienda', 'usuarios.id_tipo_vivienda', '=', 'tipos_vivienda.id_vivienda'
+    ).select(
+      'usuarios.*', 'provincias.provincia', 'tipos_vivienda.nombre_vivienda'
+    ).then(
+      data => res.send(data)
+    ))
 
-  app.get('/usuarios/:usuarioId', (req, res, next) => {
-    const { usuarioId } = req.params
-    console.log(usuarioId)
+  app.post('/usuarios/:usuario', (req, res, next) => {
+    const { usuario } = req.body
+    // bcrypt.hash(usuario.password, 10, function (hash) {
+    //   console.log(hash)
+    // })
+    knex('usuarios').insert(
+      {dni: usuario.dni},
+      {email: usuario.email},
+      {password: usuario.password},
+      {nombre: usuario.nombre},
+      {apellidos: usuario.apellidos},
+      {telefono: usuario.telefono},
+      {direccion: usuario.direccion1},
+      {fecha_nacimiento: usuario.fecha_nac},
+      {id_tipo_usuario: 2},
+      {id_provincia: usuario.provincia},
+      {direccion2: usuario.direccion2},
+      {id_tipo_vivienda: usuario.tipoVivienda}
+    )
+    console.log(usuario)
   })
 
-  app.post('/usaurios/:usuarioId', (req, res, next) => {
+  app.get('/usaurios/:usuarioId', (req, res, next) => {
 
   })
 
@@ -23,6 +50,15 @@ const apicontroller = app => {
     const { usuarioId } = req.params
     console.log(usuarioId)
   })
+
+  // Provincias
+  app.get('/provincias', (req, res, next) => knex('provincias').select('*').then(data => res.send(data)))
+
+  // Viviendas
+  app.get('/viviendas', (req, res, next) => knex('tipos_vivienda').select('*').then(data => res.send(data)))
+
+  // Caracteristicas
+  app.get('/caracteristicas', (req, res, next) => knex('caracteristicas').select('*').then(data => res.send(data)))
 }
 
 export default apicontroller
